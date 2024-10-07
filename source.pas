43f64055ad779f0c51ecaf990276fdbb74be0d68
DeRaLoRd <vastaras2004@yandex.ru>
2024-10-07 04:13:09 +0300

    add source file + minor edits

---
+++ source.pas
@@ -0,0 +1,16 @@
+﻿Program Source;
+var
+  x:integer = 2;
+  y: double = 3.22232;
+begin
+  var s : integer = 3;
+  writeln(s);
+  println(x);
+  {sd(*fs
+  f*)df}
+  
+  // sdf
+  
+  (*sd
+  fs*)
+end.
